93bf1d515502e7b29c701c1c4a30d0e9cb3e6e35
lazy-sky <1cielo@naver.com>
2022-10-09 22:57:17 +0900

    Feat: Wrap App with RecoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RecoilRoot } from 'recoil'
 import { createRoot } from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 
@@ -14,11 +15,13 @@ const queryClient = new QueryClient()
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 )
 
