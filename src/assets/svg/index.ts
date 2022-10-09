cfef6f5ff55f12f601c544af19f3559826dc4ddb
lazy-sky <1cielo@naver.com>
2022-10-09 21:36:34 +0900

    Asset: Upload icons

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as HomeIcon } from './icon_home.svg'
 export { ReactComponent as SheetIcon } from './icon_sheet.svg'
+export { ReactComponent as StarIcon } from './icon_star.svg'
+export { ReactComponent as StarFilledIcon } from './icon_star-fill.svg'
