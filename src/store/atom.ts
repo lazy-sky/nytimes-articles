c158c6bb0bb169ea67baac37bca796204e60ff2a
lazy-sky <1cielo@naver.com>
2022-10-10 17:29:41 +0900

    Feat: Make atoms about query params of api

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -8,3 +8,18 @@ export const scrappedArticlesState: RecoilState<IArticle[]> = atom({
   default: [],
   effects: [persistAtom],
 })
+
+export const headlineQueryState = atom({
+  key: 'headlineQuery',
+  default: '',
+})
+
+export const dateQueryState = atom({
+  key: 'dateQuery',
+  default: '',
+})
+
+export const nationsQueryState = atom({
+  key: 'nationsQuery',
+  default: [] as string[],
+})
