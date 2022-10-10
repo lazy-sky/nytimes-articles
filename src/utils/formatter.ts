d3646e8d81ef77e14084311bc7ce2057dbfdcd2a
lazy-sky <1cielo@naver.com>
2022-10-10 17:42:29 +0900

    Fix: Add early return logic to formatDate util

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/ko'
 
-export const formatDate = (date: string, format: string = 'YYYY.MM.DD (dd)') => {
+export const formatDate = (date: string | null, format: string = 'YYYY.MM.DD (dd)') => {
+  if (!date) return ''
+
   return dayjs(date).locale('ko').format(format)
 }
