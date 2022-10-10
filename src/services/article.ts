39e0988febff98df64d712995b60a2c504526e40
lazy-sky <1cielo@naver.com>
2022-10-10 17:36:50 +0900

    Feat: Make api function get params

--- src/services/article.ts
+++ src/services/article.ts
@@ -5,13 +5,31 @@ const nytClient = axios.create({
   params: { 'api-key': process.env.REACT_APP_API_KEY },
 })
 
-export const getArticles = async (page = 0) => {
+export const getArticles = async (page = 0, q = '', date = '', nations = [] as string[]) => {
+  const params: any = { sort: 'newest' }
+
+  if (page) {
+    params.page = page
+  }
+
+  if (q) {
+    params.q = q
+  }
+
+  if (date) {
+    params.begin_date = date
+  }
+
+  if (nations.length) {
+    params.fq = `glocations:${nations}`
+  }
+
   const {
     data: {
       response: { docs },
     },
   } = await nytClient.get('', {
-    params: { page },
+    params,
   })
 
   return { docs, nextPage: page + 1 }
