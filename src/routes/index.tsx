9ffe09688b3275ec7a373cd82ba451eb18103a9a
lazy-sky <1cielo@naver.com>
2022-10-10 20:36:26 +0900

    Feat: Set layout of App

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
+import GNB from 'components/GNB'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
 import Scrap from './Scrap'
 
+import style from './App.module.scss'
+
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/scrap' element={<Scrap />} />
-      </Routes>
+    <div style={{ background: 'gray' }}>
+      <div className={style.app}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/scrap' element={<Scrap />} />
+        </Routes>
+        <footer>
+          <GNB />
+        </footer>
+      </div>
     </div>
   )
 }
